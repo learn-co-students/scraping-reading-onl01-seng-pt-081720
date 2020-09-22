8248a6fdbe826ec6133b97c45d09b86347cb1616
Jeremy Wood <js.wood206@gmail.com>
2020-09-22 02:15:05 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,16 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+
+
+
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+puts doc.css(".headline-26OIBN").text.strip
+courses = doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+courses.each do |course|
+  binding.pry
+  puts course.text.strip
+end
